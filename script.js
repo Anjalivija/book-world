f9e56daf54590b8fe84a8e1a89a0273110d135fa
Anjali <153890033+Anjalivija@users.noreply.github.com>
2024-06-18 11:49:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// selecting the popupbox and button
+var button=document.querySelector(".btn")
+var popupBox=document.getElementById("add-popup-button")
+// function
+button.addEventListener("click",function(){
+    popupBox.style.display="block"
+
+})
